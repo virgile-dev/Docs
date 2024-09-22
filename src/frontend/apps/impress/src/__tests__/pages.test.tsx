e892d41d62b2acee083f342b8c6893ab990e1c6f
Arnaud Robin <arnaud.robin@beta.gouv.fr>
2024-09-23 01:17:13 +0200

    ✅(frontend) fix failing test
    Modification of the create doc button added the
    need for useRouter to be mounted during testing.
    This adds a mock function to allow for testing.

--- src/frontend/apps/impress/src/__tests__/pages.test.tsx
+++ src/frontend/apps/impress/src/__tests__/pages.test.tsx
@@ -5,6 +5,15 @@ import { AppWrapper } from '@/tests/utils';
 
 import Page from '../pages';
 
+// Mock the useRouter hook
+jest.mock('next/navigation', () => ({
+  useRouter() {
+    return {
+      push: jest.fn(),
+    };
+  },
+}));
+
 describe('Page', () => {
   it('checks Page rendering', () => {
     render(<Page />, { wrapper: AppWrapper });
